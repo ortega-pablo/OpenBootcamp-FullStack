e5bac0a507317153013927e7627fa4367f20ec4c
ortega-pablo <p.ortegariera@gmail.com>
2022-11-02 23:05:54 -0300

    introduccion a la programacion - tema 3

---
+++ CursoIntroducciónProgramación/EjercicioTema3/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+
+    public static void main(String[] args) {
+        
+        int resultado;
+
+        resultado = suma(5, 10, 20);
+
+        System.out.println(resultado);
+
+        Coche miCoche = new Coche();
+        miCoche.SumarPuertas();
+        System.out.println(miCoche.puertas);       
+    }
+
+    public static int suma(int a, int b, int c){
+        return a + b + c;
+    }
+
+    
+}
+
+class Coche {
+    public int puertas = 0 ;
+
+    public void SumarPuertas() {
+        this.puertas ++;
+    }
+}
